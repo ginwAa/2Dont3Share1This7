5c025468923c3014371edbabf84727df6dc2ee9c
ginwAa <1441909805@qq.com>
2023-05-10 23:34:55 +0800

    finish project4 97pts

--- src/storage/index/index_iterator.cpp
+++ src/storage/index/index_iterator.cpp
@@ -19,7 +19,15 @@ INDEXITERATOR_TYPE::IndexIterator(BufferPoolManager *bpm, B_PLUS_TREE_LEAF_PAGE_
     : bpm_(bpm), page_(page), index_(i) {}
 
 INDEX_TEMPLATE_ARGUMENTS
-INDEXITERATOR_TYPE::~IndexIterator() {}  // NOLINT
+INDEXITERATOR_TYPE::~IndexIterator() {
+  if (page_ == nullptr) {
+    return;
+  }
+  auto raw = bpm_->FetchPage(page_->GetPageId());
+  bpm_->UnpinPage(raw->GetPageId(), false);
+  raw->RUnlatch();
+  bpm_->UnpinPage(raw->GetPageId(), false);
+}  // NOLINT
 
 INDEX_TEMPLATE_ARGUMENTS
 auto INDEXITERATOR_TYPE::IsEnd() -> bool {
@@ -41,6 +49,11 @@ auto INDEXITERATOR_TYPE::operator++() -> INDEXITERATOR_TYPE & {
   if ((page_->GetSize() == index_ + 1) && (page_->GetNextPageId() != INVALID_PAGE_ID)) {
     auto nxt_raw = bpm_->FetchPage(page_->GetNextPageId());
     auto nxt = reinterpret_cast<B_PLUS_TREE_LEAF_PAGE_TYPE *>(nxt_raw->GetData());
+    auto raw = bpm_->FetchPage(page_->GetPageId());
+    bpm_->UnpinPage(raw->GetPageId(), false);
+    nxt_raw->RLatch();
+    raw->RUnlatch();
+    bpm_->UnpinPage(raw->GetPageId(), false);
     bpm_->UnpinPage(page_->GetPageId(), false);
     index_ = 0;
     page_ = nxt;
